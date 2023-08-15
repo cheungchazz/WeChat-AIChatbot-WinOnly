1d224f0a19f74affb3be629f8cc9121c4f7ca298
chazzjimel <126439838+chazzjimel@users.noreply.github.com>
2023-08-15 23:50:57 +0800

    Update openai_voice.py

--- voice/openai/openai_voice.py
+++ voice/openai/openai_voice.py
@@ -13,14 +13,16 @@ from voice.voice import Voice
 
 class OpenaiVoice(Voice):
     def __init__(self):
-        openai.api_key = conf().get("voice_ai_api_key", conf().get("open_ai_api_key", ))
-        openai.api_base = conf().get("voice_ai_api_base", conf().get("open_ai_api_base", ))
+        self.set_openai_config()
+
+    def set_openai_config(self):
+        openai.api_key = conf().get("voice_openai_api_key", conf().get("open_ai_api_key", ))
+        openai.api_base = conf().get("voice_openai_api_base", conf().get("open_ai_api_base", ))
 
     def voiceToText(self, voice_file):
         logger.debug("[Openai] voice file name={}".format(voice_file))
         try:
-            openai.api_key = conf().get("voice_ai_api_key", conf().get("open_ai_api_key", ))
-            openai.api_base = conf().get("voice_ai_api_base", conf().get("open_ai_api_base", ))
+            self.set_openai_config()
             file = open(voice_file, "rb")
             result = openai.Audio.transcribe("whisper-1", file)
             text = result["text"]
